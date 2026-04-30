52a412f7060827e0853823801516ce9104925bc5
Marco Andronaco <andronacomarco@gmail.com>
2026-04-30 10:10:50 +0200

    Make gbagfx FATAL_ERROR portable across MSVC/clang

--- tools/src/gbagfx/global.h
+++ tools/src/gbagfx/global.h
@@ -3,29 +3,24 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+static inline void fatal_error_impl(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    exit(1);
+}
+
+#define FATAL_ERROR(...) fatal_error_impl(__VA_ARGS__)
+
 #ifdef _MSC_VER
-
-#define FATAL_ERROR(format, ...)              \
-    do {                                      \
-        fprintf(stderr, format, __VA_ARGS__); \
-        exit(1);                              \
-    } while (0)
-
 #define UNUSED
-
 #else
-
-#define FATAL_ERROR(format, ...)                \
-    do {                                        \
-        fprintf(stderr, format, ##__VA_ARGS__); \
-        exit(1);                                \
-    } while (0)
-
 #define UNUSED __attribute__((__unused__))
-
 #endif // _MSC_VER
 
 #endif // GLOBAL_H
